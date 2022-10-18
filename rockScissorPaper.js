a6178db196062fea7da5f75cbeecfc63ba55ace1
FranzSmn <franzqsimon@gmail.com>
2022-10-19 01:01:13 +0800

    added the foreach loop

--- rockScissorPaper.js
+++ rockScissorPaper.js
@@ -1,4 +1,11 @@
-let paperPick = document
+let paperPick = document.getElementById('paper');
+let rockPick = document.getElementById('rock');
+let scissorsPick = document.getElementById('scissors')
+
+//query selector to select all the class with weapon name
 let playerWeapon = document.querySelectorAll('.weapon');
 
-console.log(playerWeapon);
+//For each loop to loop through each button 
+playerWeapon.forEach(weapons => {
+    weapons.onclick = () => console.log(weapons.value);
+});
